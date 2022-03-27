59047e232b401c4e905af2f050bc9aa22255df99
Brett Bryant <bbryant572@gmail.com>
2022-03-27 11:22:12 -0600

    fixed it

--- posts/models.py
+++ posts/models.py
@@ -7,12 +7,12 @@ from django.contrib.auth import get_user_model
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
-    # author = models.ForeignKey(
-    # get_user_model(),
-    # on_delete=models.CASCADE
-    # )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     text = models.TextField()
-    # created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
